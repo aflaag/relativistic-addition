f4d25c832101157037d78aa7aa516bb4f17c9014
ph04 <47356461+ph04@users.noreply.github.com>
2021-05-01 17:07:14 +0200

    Update video.py

--- video.py
+++ video.py
@@ -90,3 +90,28 @@ class VerySmallSpeed(Scene):
         text = Text("Trasformazione di Galileo")
         text.next_to(approx2, DOWN)
         self.play(Write(text, run_time=1.5))
+
+class Demonstration(GraphScene):
+    def construct(self):
+        self.x_axis_label = "$t$"
+        self.x_axis_label = "$x$"
+        
+        self.setup_axes(animate=True)
+
+        parabola = lambda x: -1/4 * x ** 2 + 4 * x - 10
+
+        graph = self.get_graph(parabola, x_min=5, x_max=8, color=RED)
+
+        # i wasn't able to draw horizontal lines
+        t1 = self.get_vertical_line_to_graph(5, graph, color=WHITE)
+        t2 = self.get_vertical_line_to_graph(8, graph, color=WHITE)
+
+        self.play(Create(graph), Create(t1), Create(t2))
+
+        sec = self.get_secant_slope_group(5, graph, dx=3) # 3 = 5 - 8, dx starts from x
+
+        text1 = MathTex("u=\\frac{\\Delta x}{\\Delta t}=\\frac{x_2-x_1}{t_2-t_1}")
+        text1.to_edge(UP)
+
+        self.play(Write(text1, run_time=2), Create(sec, run_time=2))
+        self.wait()
